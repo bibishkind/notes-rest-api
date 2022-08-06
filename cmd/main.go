3185a84d72fca6321068a157abce993f52b10cb7
bibishkind <adenshepiano@gmail.com>
2022-08-06 23:20:46 +0300

    added config

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	. "github.com/bibishkin/bi-notes-rest-api"
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 )
 
 func main() {
@@ -10,8 +11,18 @@ func main() {
 	logger.SetFormatter(new(logrus.JSONFormatter))
 	logger.SetReportCaller(true)
 
+	if err := initConfig(); err != nil {
+		logrus.Fatalf("error intializing configs: %s", err.Error())
+	}
+
 	srv := new(Server)
-	if err := srv.Run("8080111", nil); err != nil {
+	if err := srv.Run(viper.GetString("port"), nil); err != nil {
 		logger.Fatalf("error occured whlie running a http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
