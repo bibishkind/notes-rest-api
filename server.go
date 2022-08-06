15dffd47789834778f6d75d56c067338e32166cf
bibishkind <adenshepiano@gmail.com>
2022-08-06 22:20:11 +0300

    server entity

---
+++ server.go
@@ -0,0 +1,22 @@
+package bi_notes_rest_api
+
+import (
+	"context"
+	"net/http"
+)
+
+type Server struct {
+	httpServer *http.Server
+}
+
+func (s *Server) Run(port string, handler http.Handler) error {
+	s.httpServer = &http.Server{
+		Addr:    ":" + port,
+		Handler: handler,
+	}
+	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) ShutDown(ctx context.Context) error {
+	return s.httpServer.Shutdown(ctx)
+}
